3e6713bef5312aa27559a7762d35eeef79c46fd4
[jess santos] <jessxsantos@icloud.com>
2024-03-22 18:08:08 +0000

    Updating repo

---
+++ assets/js/questions.js
@@ -0,0 +1,86 @@
+const quizQuestions = [
+    {
+        question: "what is the question one?",
+
+        answers: [
+            "example one",
+            "example two",
+            "example three"
+        ],
+
+        correctAnswer: 1
+    },
+
+    {
+        question: "what is the question two?",
+
+        answers: [
+            "example one",
+            "example two",
+            "example three"
+        ],
+
+        correctAnswer: 2
+    },
+
+    {
+        question: "what is the question three?",
+
+        answers: [
+            "example one",
+            "example two",
+            "example three"
+        ],
+
+        correctAnswer: 3
+    },
+
+    {
+        question: "what is the question four?",
+
+        answers: [
+            "example one",
+            "example two",
+            "example three"
+        ],
+
+        correctAnswer: 1
+    },
+
+    {
+        question: "what is the question five?",
+
+        answers: [
+            "example one",
+            "example two",
+            "example three"
+        ],
+
+        correctAnswer: 1
+    },
+
+    {
+        question: "what is the question six?",
+
+        answers: [
+            "example one",
+            "example two",
+            "example three"
+        ],
+
+        correctAnswer: 1
+    },
+
+    {
+        question: "what is the question seven?",
+
+        answers: [
+            "example one",
+            "example two",
+            "example three"
+        ],
+
+        correctAnswer: 1
+    },
+
+]
